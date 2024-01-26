8cf7ab61a8c208590ba394cb8eddb3b193468e0e
Jonas9172 <105164575+Jonas9172@users.noreply.github.com>
2024-01-27 06:02:11 +0800

    Update dp.py

--- dp.py
+++ dp.py
@@ -16,3 +16,17 @@ for i in range(1, lastDay+1):
     else:
         dp[i] = dp[i-1]
 print(dp[lastDay])
+
+伐木工答案：
+x = int(input())
+dp = [each for each in range(0,x+1)]
+res = [[each] for each in range(0,x+1)]
+for i in range(5,x+1):
+    for j in range(1,i//2 + 1):
+        if dp[j] * dp[i-j] > dp[i] or (dp[j] * dp[i-j] == dp[i] and len(res[j]) + len(res[i-j]) < len(res[i])):
+            dp[i] = dp[j] * dp[i-j]
+            res[i].clear()
+            res[i].extend(res[j])
+            res[i].extend(res[i-j])
+    
+print(' '.join(list(map(str,sorted(res[x])))))
